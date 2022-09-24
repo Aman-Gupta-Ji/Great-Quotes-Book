e17058a38940c9ed5e92874e74c259f6ccb5d42f
Aman Gupta <amanguptaprogrammer@gmail.com>
2022-09-25 00:35:13 +0530

    All Quotes

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,13 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Aman", text: "Programmers are fun" },
+  { id: "q2", author: "Gupta Ji", text: "Is it better to speak or Die?" },
+  { id: "q3", author: "Gupta Ji", text: "Call me by your Name!" },
+];
+
 const AllQuotes = () => {
-    return <h1>All Quotes</h1>;
- };
+  return <QuoteList quotes={DUMMY_QUOTES} />;
+};
 
 export default AllQuotes;
