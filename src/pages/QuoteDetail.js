1d09803ad94c84850b450f98a89fc0be188c9550
Aman Gupta <amanguptaprogrammer@gmail.com>
2022-09-30 02:53:29 +0530

    Load Comments: Nested Route

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,4 +1,4 @@
-import { Route, useParams } from "react-router-dom";
+import { Route, useParams, Link } from "react-router-dom";
 
 import HighLightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
@@ -10,17 +10,26 @@ const DUMMY_QUOTES = [
 ];
 
 const QuoteDetail = () => {
-    const params = useParams();
-    
-    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+  const params = useParams();
 
-    if (!quote) {
-        return <p>No Quote Found</p>
-    }
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No Quote Found</p>;
+  }
 
   return (
     <>
-          <HighLightedQuote text={quote.text} author={quote.author} />          
+      <HighLightedQuote text={quote.text} author={quote.author} />
+      
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
