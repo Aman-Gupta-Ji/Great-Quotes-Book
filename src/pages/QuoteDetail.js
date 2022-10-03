82ead220d3422f35cebe78224b59fea2eac299ab
Aman Gupta <amanguptaprogrammer@gmail.com>
2022-10-04 04:58:36 +0530

    Quote Detail View

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,27 +1,52 @@
+import { Fragment, useEffect } from "react";
 import { Route, useParams, Link, useRouteMatch } from "react-router-dom";
 
 import HighLightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
-
-const DUMMY_QUOTES = [
-  { id: "q1", author: "Aman", text: "Programmers are fun" },
-  { id: "q2", author: "Gupta Ji", text: "Is it better to speak or Die?" },
-  { id: "q3", author: "Gupta Ji", text: "Call me by your Name!" },
-];
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
 const QuoteDetail = () => {
   const match = useRouteMatch();
   const params = useParams();
 
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  const { quoteId } = params;
 
-  if (!quote) {
-    return <p>No Quote Found</p>;
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="centered focused">
+        <p>{error}</p>
+      </div>
+    );
+  }
+
+  if (!loadedQuote.text) {
+    return <p>No quote found!</p>;
   }
 
   return (
-    <>
-      <HighLightedQuote text={quote.text} author={quote.author} />
+    <Fragment>
+      <HighLightedQuote text={loadedQuote.text} author={loadedQuote.author} />
 
       <Route path={match.path} exact>
         <div className="centered">
@@ -34,7 +59,7 @@ const QuoteDetail = () => {
       <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
-    </>
+    </Fragment>
   );
 };
 
